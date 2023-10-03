bdb2f7f7a05b352f0c2407ff3e538f11db0ae956
Jocelyn <jocelyntan89@hotmail.com>
2023-10-03 20:37:19 +0200

    refactor: from composition to options API

--- src/components/ThreeDObjectGenerator.vue
+++ src/components/ThreeDObjectGenerator.vue
@@ -2,125 +2,108 @@
   <canvas ref="canvas" />
 </template>
 
-<script setup>
+<script>
 import * as THREE from 'three'
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
-import { ref, onMounted, watch } from 'vue'
 
-const props = defineProps({
-  geometry: Array,
-  editableHeight: Number
-})
+export default {
+  props: {
+    geometry: Array,
+    editableHeight: Number
+  },
+  watch: {
+    editableHeight(newHeight) {
+      this.mesh.scale.z = newHeight
+    }
+  },
+  mounted() {
+    this.sizes = {
+      width: window.innerWidth,
+      height: window.innerHeight
+    }
+    this.canvas = this.$refs.canvas
+    this.scene = new THREE.Scene()
+    this.extrudeGeometry = this.createExtrudeGeometry()
+    this.mesh = this.createMesh()
+    this.camera = this.createCamera()
+    this.renderer = this.createRenderer()
+    this.createControls()
+    this.addResizeListener()
+    this.setAnimation()
+  },
+  methods: {
+    createExtrudeGeometry() {
+      const shape = new THREE.Shape()
+      const startingPoint = this.geometry[0]
 
-const sizes = {
-  width: window.innerWidth,
-  height: window.innerHeight
-}
-const canvas = ref(null)
-const scene = new THREE.Scene()
-const extrudeGeometry = createExtrudeGeometry()
-const mesh = createMesh()
-const camera = createCamera()
-let renderer
-createAxis()
+      shape.moveTo(startingPoint[0], startingPoint[1])
+      for (let i = 1; i < this.geometry.length; i++) {
+        const point = this.geometry[i]
+        shape.lineTo(point[0], point[1])
+      }
+      shape.lineTo(startingPoint[0], startingPoint[1])
 
-onMounted(() => {
-  renderer = createRenderer()
-  createControls()
-  addResizeListener()
-  setAnimation()
-})
+      const extrudeSettings = {
+        steps: 1,
+        depth: this.editableHeight,
+        bevelEnabled: false
+      }
 
-watch(
-  () => props.editableHeight,
-  (newHeight) => {
-    mesh.scale.z = newHeight
+      const extrudeGeometry = new THREE.ExtrudeGeometry(shape, extrudeSettings)
+
+      return extrudeGeometry
+    },
+    createMesh() {
+      const materialTop = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+      const materialSides = new THREE.MeshBasicMaterial({ color: 'blue' })
+      const mesh = new THREE.Mesh(this.extrudeGeometry, [materialTop, materialSides])
+      mesh.rotation.x = - Math.PI / 2
+      mesh.geometry.center()
+      this.scene.add(mesh)
+
+      return mesh
+    },
+    createCamera() {
+      const fov = 75
+      const aspect = this.sizes.width / this.sizes.height
+      const camera = new THREE.PerspectiveCamera(fov, aspect)
+      camera.position.x = 2
+      camera.position.y = 2
+      camera.position.z = 10
+      camera.lookAt(this.mesh.position)
+      this.scene.add(camera)
+
+      return camera
+    },
+    createRenderer() {
+      const renderer = new THREE.WebGLRenderer({ antialias: true, canvas: this.canvas })
+      renderer.setSize(this.sizes.width, this.sizes.height)
+      renderer.render(this.scene, this.camera)
+
+      return renderer
+    },
+    createControls() {
+      const controls = new OrbitControls(this.camera, this.canvas)
+      controls.enableDamping = true
+    },
+    addResizeListener() {
+      window.addEventListener('resize', () => {
+        // Update sizes
+        this.sizes.width = window.innerWidth
+        this.sizes.height = window.innerHeight
+
+        // Update camera
+        this.camera.aspect = this.sizes.width / this.sizes.height
+        this.camera.updateProjectionMatrix()
+
+        // Update renderer
+        this.renderer.setSize(this.sizes.width, this.sizes.height)
+      })
+    },
+    setAnimation() {
+      this.renderer.render(this.scene, this.camera)
+      window.requestAnimationFrame(this.setAnimation)
+    }
   }
-)
-
-function createExtrudeGeometry() {
-  const shape = new THREE.Shape()
-  const startingPoint = props.geometry[0]
-
-  shape.moveTo(startingPoint[0], startingPoint[1])
-  for (let i = 1; i < props.geometry.length; i++) {
-    const point = props.geometry[i]
-    shape.lineTo(point[0], point[1])
-  }
-  shape.lineTo(startingPoint[0], startingPoint[1])
-
-  const extrudeSettings = {
-    steps: 1,
-    depth: props.editableHeight,
-    bevelEnabled: false
-  }
-
-  const extrudeGeometry = new THREE.ExtrudeGeometry(shape, extrudeSettings)
-
-  return extrudeGeometry
-}
-
-function createMesh() {
-  const materialTop = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-  const materialSides = new THREE.MeshBasicMaterial({ color: 'blue' })
-  const mesh = new THREE.Mesh(extrudeGeometry, [materialTop, materialSides])
-  mesh.rotation.x = - Math.PI / 2
-  mesh.geometry.center()
-  scene.add(mesh)
-
-  return mesh
-}
-
-function createCamera() {
-  const fov = 75
-  const aspect = sizes.width / sizes.height
-  const camera = new THREE.PerspectiveCamera(fov, aspect)
-  camera.position.x = 2
-  camera.position.y = 2
-  camera.position.z = 10
-  camera.lookAt(mesh.position)
-  scene.add(camera)
-
-  return camera
-}
-
-function createAxis() {
-  const axesHelper = new THREE.AxesHelper(2)
-  scene.add(axesHelper)
-}
-
-function createRenderer() {
-  const renderer = new THREE.WebGLRenderer({ antialias: true, canvas: canvas.value })
-  renderer.setSize(sizes.width, sizes.height)
-  renderer.render(scene, camera)
-
-  return renderer
-}
-
-function createControls() {
-  const controls = new OrbitControls(camera, canvas.value)
-  controls.enableDamping = true
-  
-  return controls
-}
-
-function addResizeListener() {
-  window.addEventListener('resize', () => {
-    // Update sizes
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
-
-    // Update camera
-    camera.aspect = sizes.width / sizes.height
-    camera.updateProjectionMatrix()
-
-    // Update renderer
-    renderer.setSize(sizes.width, sizes.height)
-  })
-}
-
-function setAnimation() {
-  renderer.render(scene, camera)
-  window.requestAnimationFrame(setAnimation)
 }
 </script>
