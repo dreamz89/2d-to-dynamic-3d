2222f9049d3b403f68e59abe9ae3c8e900f708bc
Jocelyn <jocelyntan89@hotmail.com>
2023-10-03 20:38:28 +0200

    feat: add gui controls

--- src/components/GeometryOne.vue
+++ src/components/GeometryOne.vue
@@ -1,10 +1,9 @@
 <template>
-  <div>
-    <ThreeDObjectGenerator :geometry="GEOMETRY" :editableHeight="height" />
-  </div>
+  <ThreeDObjectGenerator :geometry="GEOMETRY" :editableHeight="height" />
 </template>
 
 <script>
+import { GUI } from 'dat.gui'
 import ThreeDObjectGenerator from '@/components/ThreeDObjectGenerator.vue'
 
 export default {
@@ -18,6 +17,15 @@ export default {
   },
   created() {
     this.GEOMETRY = [[0,0],[0,10],[15,10],[7,15],[7,8],[7,0]]
+
+    this.createGui()
+  },
+  methods: {
+    createGui() {
+      const gui = new GUI()
+      gui.domElement.id = 'gui'
+      gui.add(this, 'height').min(0).max(5).step(1)
+    }
   }
 }
 </script>
--- src/components/GeometryTwo.vue
+++ src/components/GeometryTwo.vue
@@ -1,10 +1,9 @@
 <template>
-  <div>
-    <ThreeDObjectGenerator :geometry="GEOMETRY" :editableHeight="height" />
-  </div>
+  <ThreeDObjectGenerator :geometry="GEOMETRY" :editableHeight="height" />
 </template>
 
 <script>
+import { GUI } from 'dat.gui'
 import ThreeDObjectGenerator from './ThreeDObjectGenerator.vue'
 
 export default {
@@ -19,13 +18,14 @@ export default {
   created() {
     this.GEOMETRY = [[0,0], [7,12], [0,14]]
 
-    setTimeout(() => {
-      this.height = 2
-    }, 1000)
-
-    setTimeout(() => {
-      this.height = 1
-    }, 2000)
+    this.createGui()
+  },
+  methods: {
+    createGui() {
+      const gui = new GUI()
+      gui.domElement.id = 'gui'
+      gui.add(this, 'height').min(0).max(5).step(1)
+    }
   }
 }
 </script>
